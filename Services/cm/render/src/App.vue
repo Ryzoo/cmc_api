e9627f7ae601245da64c90245e4bb22b8091accd
Grzegorz Kastelik <g.kastelik@gmail.com>
2021-05-29 16:34:19 +0200

    asd

--- Services/cm/render/src/App.vue
+++ Services/cm/render/src/App.vue
@@ -28,10 +28,14 @@
 			if(window.apiData){
 				axios.get(`https://api.centrumklubu.pl/animations/${window.apiData.id}`)
 					.then((response)=>{
+						console.log(response)
                         const watermark = (response.data.watermark && response.data.watermark.length > 3) ? JSON.parse(response.data.watermark) : null;
                         animEngine.build(true,{watermark: watermark, name: response.data.name, pathField:response.data.path_field , frameData: JSON.parse(response.data.frame_data), objectInAnimation: JSON.parse(response.data.object_in_animation)});
 						$(window).on('resize',this.resizeLayout);
 						$("#allFrame").val(-1);
+					})
+					.catch((response)=>{
+						console.log(response)
 					})
 			}
 		}

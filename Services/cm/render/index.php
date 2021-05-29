9b0df1bd59c6363c0e96f99639ad6e6754e7ff06
Grzegorz Kastelik <g.kastelik@gmail.com>
2021-05-29 17:26:40 +0200

    sad

--- Services/cm/render/index.php
+++ Services/cm/render/index.php
@@ -11,9 +11,10 @@
 		window.apiData = JSON.parse('<?php echo json_encode(array("id"=>$_GET["id"],"token"=>$_GET["token"])) ?>');
 	</script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/native-promise-only/0.8.1/npo.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-            crossorigin="anonymous"></script>
+  <script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"></script>
   <script src="https://zimjs.org/cdn/1.3.3/createjs.js"></script>
   <script src="https://zimjs.org/cdn/cat/04/zim.js"></script>

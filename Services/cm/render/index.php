640199327a13c7b5f677c736b93e2424a6e1bcf3
Grzegorz Kastelik <g.kastelik@gmail.com>
2021-05-29 17:29:27 +0200

    sad

--- Services/cm/render/index.php
+++ Services/cm/render/index.php
@@ -7,7 +7,7 @@
   </head>
   <body>
     <div id="app"></div>
-	<script>
+	<script defer>
 		window.apiData = JSON.parse('<?php echo json_encode(array("id"=>$_GET["id"],"token"=>$_GET["token"])) ?>');
 	</script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/native-promise-only/0.8.1/npo.js"></script>
@@ -16,9 +16,8 @@
   integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
   crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"></script>
-  <script src="https://zimjs.org/cdn/1.3.3/createjs.js"></script>
-  <script src="https://zimjs.org/cdn/cat/04/zim.js"></script>
+  <script src="https://zimjs.org/cdn/1.1.0/createjs.js"></script>
+  <script src="https://zimjs.org/cdn/7.3.0/zim.js"></script>
   <script src="dist/build.js"></script>
-
   </body>
 </html>
